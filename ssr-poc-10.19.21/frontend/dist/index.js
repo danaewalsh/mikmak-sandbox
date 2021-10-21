fab915848a4e9129dc8ce73de4dcb82108ff1ef6
Dana Walsh <danawalsh@Danas-MacBook-Pro.local>
2021-10-21 13:26:00 -0700

    POC complete. Player is rendering with fully functionality using element.insertAdjacentHTML

--- ssr-poc-10.19.21/frontend/dist/index.js
+++ ssr-poc-10.19.21/frontend/dist/index.js
@@ -1,8 +1,16 @@
 // get the element
 let app = document.getElementById('app');
+let url = 'http://localhost:3000'
+let embeddedURL = 'http://localhost:5005/embedded/link/23578ca6-3b08-56a4-2e33-88886a8a85ad?internalVisualTest=true&cb=5477'
 
 // axios request to get the ejs template
-axios.get('http://localhost:3000')
+axios.get(embeddedURL)
     .then(({ data }) => {
-        app.innerHTML = data;
+        app.insertAdjacentHTML('beforeend', data)
+    })
+    .then(() => {
+        let jsScript = document.createElement('script');
+        jsScript.type = "text/javascript"
+        jsScript.src = "http://localhost:5005/ssr.js"
+        app.appendChild(jsScript);
     })
